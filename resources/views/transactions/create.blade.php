15cf0ea64be7d88301abbe0a529e102ba4f9ddd2
towakey <kyosgame@gmail.com>
2025-02-06 21:38:22 +0900

    商品・店舗登録モーダルのボタン修正

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -88,11 +88,11 @@
                         <x-text-input id="store_name" type="text" class="mt-1 block w-full" required />
                     </div>
                     <div class="mt-4 flex justify-end">
-                        <button type="button" id="closeStoreModalBtn" class="mr-2 px-4 py-2 text-gray-500 hover:text-gray-700">
+                        <button type="button" id="closeStoreModalBtn" class="mr-2 px-4 py-2 bg-gray-100 text-gray-700 rounded hover:bg-gray-200 border border-gray-300">
                             キャンセル
                         </button>
-                        <button type="button" id="submitStoreBtn" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                            登録
+                        <button type="button" id="submitStoreBtn" class="px-4 py-2 bg-red-600 text-white font-semibold rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 border border-red-700 shadow-sm">
+                            登録する
                         </button>
                     </div>
                 </form>
@@ -120,11 +120,11 @@
                         <x-text-input id="product_default_price" type="number" class="mt-1 block w-full" />
                     </div>
                     <div class="mt-4 flex justify-end">
-                        <button type="button" id="closeProductModalBtn" class="mr-2 px-4 py-2 text-gray-500 hover:text-gray-700">
+                        <button type="button" id="closeProductModalBtn" class="mr-2 px-4 py-2 bg-gray-100 text-gray-700 rounded hover:bg-gray-200 border border-gray-300">
                             キャンセル
                         </button>
-                        <button type="button" id="submitProductBtn" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                            登録
+                        <button type="button" id="submitProductBtn" class="px-4 py-2 bg-red-600 text-white font-semibold rounded hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 border border-red-700 shadow-sm">
+                            登録する
                         </button>
                     </div>
                 </form>
